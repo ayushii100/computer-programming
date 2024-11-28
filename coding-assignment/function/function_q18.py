5d291616d26d0a432c84ebf4cc4e6fc2a4af07f2
ayushii100 <bansal.ayushi578@gmail.com>
2024-11-28 20:40:13 +0530

    Create function_q18.py

---
+++ coding-assignment/function/function_q18.py
@@ -0,0 +1,8 @@
+# Function for File Operations
+def write_and_read_file(file_name, text):
+    with open(file_name, "w") as file:
+        file.write(text)
+    with open(file_name, "r") as file:
+        return file.read()
+
+print(write_and_read_file("example.txt", "Hello, File!"))
