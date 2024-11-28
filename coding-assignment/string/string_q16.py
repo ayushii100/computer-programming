eae97aee03ed04ed0f6458bd5675f96d8581adf9
ayushii100 <bansal.ayushi578@gmail.com>
2024-11-28 09:07:34 +0530

    Create string_q16.py

---
+++ coding-assignment/string/string_q16.py
@@ -0,0 +1,4 @@
+#Counting occurence of a substring
+sentence = "apple orange apple banana apple grape"
+count_apples = sentence.count("apple")
+print("The word 'apple' appears:", count_apples, "times.")
