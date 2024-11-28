851998f9e822ffedc13086d712d41133e5d5939c
ayushii100 <bansal.ayushi578@gmail.com>
2024-11-28 14:56:02 +0530

    Create list_q20.py

---
+++ coding-assignment/list/list_q20.py
@@ -0,0 +1,5 @@
+# Find the Intersection of Two Lists
+list1 = [1, 2, 3, 4, 5]
+list2 = [4, 5, 6, 7, 8]
+intersection = [num for num in list1 if num in list2]
+print("Intersection of the two lists:", intersection)
