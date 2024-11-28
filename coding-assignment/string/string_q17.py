30255c7fb7798c1f4268f5be92a878235ccb5cc7
ayushii100 <bansal.ayushi578@gmail.com>
2024-11-28 09:08:33 +0530

    Create string_q17.py

---
+++ coding-assignment/string/string_q17.py
@@ -0,0 +1,6 @@
+#Checking string properties
+text = "Hello123"
+alpha_numeric = text.isalnum()  # True if all characters are alphanumeric
+only_alpha = text.isalpha()     # True if all characters are alphabetic
+only_digits = "123456".isdigit()  # True if all characters are digits
+space_check = "   ".isspace() 
