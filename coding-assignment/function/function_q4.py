0a6cfd3bb6e5aebb2e27025a19c5e5ccb69ba2b4
ayushii100 <bansal.ayushi578@gmail.com>
2024-11-28 19:13:53 +0530

    Create function_q4.py

---
+++ coding-assignment/function/function_q4.py
@@ -0,0 +1,8 @@
+# program to count vowel and consonant 
+def count_vowels_consonants(s):
+    vowels = "aeiouAEIOU"
+    s = s.replace(" ", "")  # Remove spaces
+    vowel_count = sum(1 for char in s if char in vowels)
+    consonant_count = sum(1 for char in s if char.isalpha() and char not in vowels)
+    return vowel_count, consonant_count
+
